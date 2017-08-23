e7dde068e215579a1e8c64c455e6873b90a77855
Mike Petruniak <mike.petruniak@gmail.com>
2017-08-22 21:54:33 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,26 @@
+# Docked Link jQuery Plugin
+This jQuery plugin adds a fixed link below the fold anchored to either the center-left or center-right of your browser window. Simply specify the element/link, how far below the fold you'd like your link to appear (where the tripwire is), and which side of the browser window you'd like your link docked on.
+
+[DEMO](http://mykisscool.github.io/docked-link/)
+
+### Basic requirements
+
+- You'll need a doctype declaration or `$(window).height()` will not provide an accurate calculation.
+- The object calling the `dockedLink()` method *should* be a block-level element.
+- jQuery.
+
+### Example one
+
+```javascript
+// Default dock is on the right and the default starting point is 1,000 pixels from the top
+$('#myLink').dockedLink();
+```
+
+### Example two
+
+```javascript
+$('#myLink').dockedLink({
+  pixelsFromTop: 1500, // Longer pages
+  position: 'left'
+}).attr('href', 'https://www.github.com'); // Some chaining
+```
---
+++ docked-link.js
@@ -0,0 +1,65 @@
+/*!
+ * jQuery Docked Link Plugin
+ * https://github.com/mykisscool/docked-link
+ *
+ * Copyright 2017 Mike Petruniak
+ * Released under the MIT license
+ */
+;(function ($) {
+
+  $.fn.dockedLink = function (options) {
+
+    var defaults = {
+      pixelsFromTop: 1000, /* Magic number; somewhere below the fold */
+      position: 'right'
+    },
+    options = $.extend(defaults, options);
+
+    var $elem = this,
+        totalPageHeight = $(document).height(),
+        viewPortHalfHeight = $(window).height() / 2,
+        quickQuoteButtonHalfHeight = Math.round($elem.height() / 2);
+
+    // Right or left
+    $elem.css(options.position, 0);
+
+    function positionQuickQuote () {
+
+        // Short-length pages
+        if (options.pixelsFromTop > totalPageHeight) {
+            $elem.css({
+              display: 'block',
+              position: 'fixed',
+              top: viewPortHalfHeight - quickQuoteButtonHalfHeight
+            });
+        }
+        // Long-length pages
+        else {
+          $elem.css({
+            display: 'block',
+            top: options.pixelsFromTop
+          });
+
+        if ($(window).scrollTop() >= (options.pixelsFromTop - (viewPortHalfHeight - quickQuoteButtonHalfHeight))) {
+            $elem.css({
+                position: 'fixed',
+                top: viewPortHalfHeight - quickQuoteButtonHalfHeight
+            });
+        }
+        else {
+          $elem.css({
+              position: 'absolute',
+              top: options.pixelsFromTop
+          });
+        }
+      }
+    }
+
+    $(window).scroll(function () {
+        positionQuickQuote();
+    });
+
+    positionQuickQuote();
+    return this;
+  };
+})(jQuery);
